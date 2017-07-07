b3734a71f3c4a89375700784e0a316f074992968
tueseed <tue_seed@hotmail.com>
2017-07-07 20:42:08 +0700

    Update cn.php

--- cn.php
+++ cn.php
@@ -27,8 +27,8 @@ if (!is_null($events['events'])) {
 			 
                 // Build message to reply back
 			$messages = [
-				//'type' => 'text',
-				//'text' => $Myd    //."  [".$KVA." KVA]"
+				'type' => 'text',
+				'text' => "dfdkakl;j"//$Myd    //."  [".$KVA." KVA]"
 			];
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
