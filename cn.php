af6cb9e78349201bf633337b7d25dd02dceec41e
tueseed <tue_seed@hotmail.com>
2017-07-09 14:00:32 +0700

    Update cn.php

--- cn.php
+++ cn.php
@@ -28,16 +28,8 @@ if (!is_null($events['events'])) {
 			 if($Myd==1){ 
 			    break;
 			}
-			 if ($Myd=="")
-				 $Myd = "สวัสดีครับ มาตรฐานการก่อสร้างระบบจำหน่ายเป็นเรื่องสำคัญ สงสัยเรื่องไหนเรามีคำตอบ พิมพ์ แรงสูง หม้อแปลง แรงต่ำ หรือคลิกที่ลิ้ง เพื่อดูวิธีการใช้งาน 
-https://drive.google.com/open?id=0B4BTAaYsG0CpMF9VRUExRWhZLUU 
-พบปัญหาการใช้งานหรือมีคำติชม ข้อเสนอแนะเพิ่มเติมติดต่อ 
-นายนัทธพงศ์ เจริญกิจพิเชียร 
-วิศวกรระดับ 5 ผกส.กฟอ.พธร. 
- เบอร์ดาวเทียม 14850 
-LINE : tueseed 
-email : nattapong.cha@pea.co.th,tue_seed@hotmail.com 
-หรือ โทร 095-5579848";
+			 if ($Myd==""){
+				 
                 // Build message to reply back
 			$messages = array(
       'type'=> 'template',
@@ -58,19 +50,13 @@ email : nattapong.cha@pea.co.th,tue_seed@hotmail.com
       )
      )
      )
-        );      //[
-				//'type' => 'text',
-				//'text' => $Myd    //."  [".$KVA." KVA]"
-				
-				
-				
-				
-				
-				
-				
-				
-				
-			//];
+        );   
+			 }
+			$messages[
+				'type' => 'text',
+				'text' => $Myd    //."  [".$KVA." KVA]"
+						
+			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
