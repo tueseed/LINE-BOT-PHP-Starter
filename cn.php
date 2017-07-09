67e994e3134946401b2e743e30ed0120548d7d13
tueseed <tue_seed@hotmail.com>
2017-07-09 14:21:16 +0700

    Update cn.php

--- cn.php
+++ cn.php
@@ -46,7 +46,7 @@ if (!is_null($events['events'])) {
                                                                 )
                                                              )
                                                             )
-                                            );   
+                                            );;   
 			
 			 }
 			else{
@@ -54,7 +54,7 @@ if (!is_null($events['events'])) {
 				'type' => 'text',
 				'text' => $Myd    //."  [".$KVA." KVA]"
 						
-			];
+			];;
 			}
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/reply';
