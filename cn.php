4f001010e8e0cace13f75b1c71e5755bc1c6e3da
tueseed <tue_seed@hotmail.com>
2017-08-04 22:32:03 +0700

    Update cn.php

--- cn.php
+++ cn.php
@@ -110,7 +110,7 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-$Ti = date("H:i:s");
+$Ti = date("H:i:s",mktime(date("H")+7, date("i")+0, date("s")+0));
 $Da = date("d.m.y");
 $strFileName = "cndis.txt";
 $objFopen = fopen($strFileName, 'a');
@@ -119,3 +119,4 @@ $strText1 = "\n\r"."  ".$Da."  ".$Ti."  ".$findName1;
 fwrite($objFopen, $strText1);
 fclose($objFopen);
 echo "OK";
+?>
