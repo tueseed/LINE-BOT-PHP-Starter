75f2cfbb0853d733be89e554cd686fc9540bd6b3
tueseed <tue_seed@hotmail.com>
2017-08-04 20:20:25 +0700

    Update wtxt.php

--- wtxt.php
+++ wtxt.php
@@ -1,8 +1,9 @@
 <?php
+$Ti = date("H:i:s");
+$Da = date("d.m.y");
 $strFileName = "cndis.txt";
 $objFopen = fopen($strFileName, 'a');
-$strText1 = "\n\rI Love ThaiCreate.Com Line1";
+$strText1 = "\n\r"."  ".$Ti."  ".$Da;
 fwrite($objFopen, $strText1);
-
 fclose($objFopen);
 ?>
