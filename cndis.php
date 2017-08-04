8ff346259fb2b63784023f700744ca9cfa204bea
tueseed <tue_seed@hotmail.com>
2017-08-04 22:49:33 +0700

    Update cndis.php

--- cndis.php
+++ cndis.php
@@ -3,7 +3,7 @@ $file = "cndis.txt"; //Let say If I put the file name Bang.png
 echo "<a href='download.php?nama=".$file."'>donload</a> ";
 $Ti = date("H:i:s",mktime(date("H")+7, date("i")+0, date("s")+0));
 $Da = date("d.m.y");
-echo "\nสถานะ." เวลา  ".$Ti."  วันที่  ".$Da;
+echo "\nสถานะ:"." เวลา  ".$Ti."  วันที่  ".$Da."\n";
 
 
 $text = file('cndis.txt');
