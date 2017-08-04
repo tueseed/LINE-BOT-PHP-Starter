cc49f6ab7e2df43208aafc785ebe7f2f387bf01c
tueseed <tue_seed@hotmail.com>
2017-08-04 20:12:06 +0700

    Update wtxt.php

--- wtxt.php
+++ wtxt.php
@@ -3,7 +3,7 @@ $Ti = date("H:i:s");
 $Da = date("d.m.y");
 $wfile = fopen("cndis.txt,"a+");or die("Unable to open file!");
 $txt = $Ti.$Da."\n";
-fwrite($wfile, $txt);
+fwrite($wfile,$txt);
 fclose($wfile);
 echo "123456789";
 ?>
