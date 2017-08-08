1b87e8b22af416bbb0f69332025d18c1410da681
tueseed <tue_seed@hotmail.com>
2017-08-08 20:30:20 +0700

    Update cndis.php

--- cndis.php
+++ cndis.php
@@ -4,7 +4,7 @@
 </head>
 <body>
 <?php
-file = "cndis.csv"; //Let say If I put the file name Bang.png
+$file = "cndis.csv"; 
 echo "<a href='download.php?nama=".$file."'>donload</a> ";
 $Ti = date("H:i:s",mktime(date("H")+7, date("i")+0, date("s")+0));
 $Da = date("d.m.y");
