7071c2a95159a6753b26582eefa1252322e60647
tueseed <tue_seed@hotmail.com>
2017-08-08 20:54:41 +0700

    Update cn.php

--- cn.php
+++ cn.php
@@ -126,7 +126,7 @@ $Da = date("d.m.y");
 $strFileName = "cndis.csv";
 $objFopen = fopen($strFileName, 'a');
 $findName1 = iconv("tis-620","utf-8",$findName);
-$strText1 = "\n\r".$Da.",".$Ti.",".$findName1.",".$userid;
+$strText1 = $Da.",".$Ti.",".$findName1.",".$userid;
 fwrite($objFopen, $strText1);
 fclose($objFopen);
 
