ac9b3ac8c3a0f199f88486ab4593c28832b3771d
tueseed <tue_seed@hotmail.com>
2017-08-16 12:15:11 +0700

    Update cn.php

--- cn.php
+++ cn.php
@@ -33,7 +33,7 @@ if (!is_null($events['events'])) {
 	     }
 			                                         }
 			//สร้างข้อความภาคผนวก
-			if($Myd=="test"){
+			if($findname=="test"){
 				$messages = array('type'=> 'template','altText'=> 'กรุณาเลือกภาคผนวก',
 				               'template'=>array('type'=>'buttons','text'=>'กรุณาเลือกภาคผนวก',
 				                         'actions'=>array('type'=> 'message','label'=> 'ภาคผนวก ก','text'=> 'ภาคผนวก ก')
